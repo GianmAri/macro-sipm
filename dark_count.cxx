f7f5cf5a18fec21fa592cc61f2f9567f8ccfe6bc
GianmAri <63963668+GianmAri@users.noreply.github.com>
2022-03-27 18:09:44 +0200

    Update dark_count.cxx

--- dark_count.cxx
+++ dark_count.cxx
@@ -6,22 +6,42 @@ void dark_cout() {
 
     
     double constant_of_conversion = 2.0/4096.0;
-    double deltat = 0.004; // us
+    //double deltat = 0.004/16.0; // us
     double resistance = 50; // Ohm
     
     Int_t temp = 0;
-    Double_t time[16*1024];
+    Int_t a[16*1024];
+    Int_t dark_cont[100000];
+    //Double_t time[16*1024];
     Int_t count = 0;
-    Double_t mean = 1.64408e-11;
-    Double_t sigma = 1.52515e-12;
+    Double_t mean = 1.64408e-11; //valore primo picco
+    Double_t sigma = 1.52515e-12; //valore primo picco
     Double_t constant_of_conversion = 4096.0/2.0;
     Double_t minimum = (mean + 1.0) * constant_of_conversion;
 
     Double_t threshold = minimum -  (3.0 * sigma);
+    
+    Int_t j = 0;
 
-
-    for(int i = 0; i < 16 * 1024; i++) {
-        file >> temp;
+    while (file.good()) {
+        count = 0;
+        for(int i = 0; i < 16 * 1024; i++) {
+            file >> temp;
+            a[i] = temp;
+            //time[i] = deltat * i;     
+            }
+        
+        for (int i = 0; i < 16 * 1024; i++) {
+            if (a[i] < threshold && (i+1) != (16 * 1024 + 1)) {
+                if(a[i+1] > a[i]) {
+                    count++;
+                }
+            }
+        }
+        dark_cont[j] = count;
+        j++;
+           
+        
         
     }
 
